400357e5f20db11545b2d0523a36cbc023895c8a
Skip Taylor <skiptaylor101@gmail.com>
2022-12-16 08:37:30 -0500

    last upgrades

--- settings/redirects.rb
+++ settings/redirects.rb
@@ -1,11 +1,11 @@
 configure :production do
 	before do
 		
-    # unless request.request_method == 'POST'
-#       unless request.url.include? "https://www."
-#         redirect "https://www.counselorexams.com#{request.path}"
-#       end
-#     end
+    unless request.request_method == 'POST'
+      unless request.url.include? "https://www."
+        redirect "https://www.counselorexams.com#{request.path}"
+      end
+    end
 		
 	end
 end
